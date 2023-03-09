9998ba3074683817e2b691c6385d7b28b35bee31
RubenTanner <ruben.sgtech@gmail.com>
2023-03-09 14:40:38 +0000

    oh lord its complicated

--- webpages/main.js
+++ webpages/main.js
@@ -1,23 +1,30 @@
 import { Pet } from "./pet.js";
 let pet;
+let currentState;
 
-  // Method to update the pet's stats when the player interacts with the pet
-  setUpdate(hunger, sleep, happiness) {
-    this.hunger += hunger;
-    this.sleep += sleep;
-    this.happiness += happiness;
+function updatePetImage() {
+  let imageSrc = "";
+  if (currentState === "feeding") {
+    imageSrc = "assets/eating.svg";
+  } else if ((currentState = "hungry")) {
+    imageSrc = "assets/hungry.svg";
+  } else if (currentState === "happy") {
+    imageSrc = "assets/happy.svg";
+  } else if (currentState === "sad") {
+    imageSrc = "assets/sad.svg";
+  } else if ((currentState = "playing")) {
+    imageSrc = "assets/playing.svg";
+  } else {
+    imageSrc = "assets/idle.svg";
   }
-
-}
-
+  petDiv.innerHTML = `<img src="${imageSrc}" alt="pet">`;
 }
 
 //init function
 function init() {
   //Add event listeners for the buttons on the page
-  document.querySelector("#submit").addEventListener("click", () => {
-    playGame();
-  });
+  const submitBtn = document.querySelector("#submit");
+  submitBtn.addEventListener("click", playGame);
   document.querySelector("#eat").addEventListener("click", () => {
     feed();
   });
@@ -30,6 +37,9 @@ function init() {
 }
 // Function to start the game
 function playGame() {
+  currentState = "playing";
+  const petDiv = document.getElementById("pet-div");
+
   // Create a new pet
   const name = document.querySelector("#pet-name").value;
   const owner = document.querySelector("#owner").value;
@@ -37,7 +47,8 @@ function playGame() {
     alert("Please enter a name and owner for your pet.");
     return;
   }
-  let pet = new Pet(name, owner);
+  pet = new Pet(name, owner); // Create a new pet
+
   console.log(pet);
   // Update the pet's stats on the page
   document.querySelector("#pet-name").textContent = pet.name;
@@ -45,27 +56,32 @@ function playGame() {
   document.querySelector("#sleep").textContent = pet.sleep;
   document.querySelector("#happiness").textContent = pet.happiness;
   setTimeout(updatePet, 1000);
+
+  function updatePet() {
     if (pet.isAlive()) {
       pet.setDecay();
       pet.setUpdate();
     } else {
-      document.querySelector("#dead").innerHTML = "Your pet is dead!";
+      document.querySelector("#dead").textContent = "Your pet is dead!";
     }
-  }, 1000);
+  }
 }
 // Function to feed the pet
 function feed() {
+  currentState = "feeding";
   // Update the pet's hunger and happiness stats
-  pet.setUpdate(-1, 0, 1);
+  pet.setUpdate(1, 0, 1);
 
   // Update the pet's stats on the page
   document.querySelector("#hunger").textContent = pet.hunger;
   document.querySelector("#happiness").textContent = pet.happiness;
 
+  updatePetImage();
 }
 
 // Function to put the pet to sleep
 function sleep() {
+  currentState = "sleeping";
   // Update the pet's sleep and happiness stats
   pet.setUpdate(0, 2, 1);
 
@@ -73,10 +89,12 @@ function sleep() {
   document.querySelector("#sleep").textContent = pet.sleep;
   document.querySelector("#happiness").textContent = pet.happiness;
 
+  updatePetImage();
 }
 
 // Function to play with the pet
 function play() {
+  currentState = "playing";
   // Update the pet's hunger and happiness stats
   pet.setUpdate(-1, 0, 3);
 
@@ -84,6 +102,8 @@ function play() {
   document.querySelector("#hunger").textContent = pet.hunger;
   document.querySelector("#happiness").textContent = pet.happiness;
 
+  updatePetImage();
 }
 
-window.addEventListener("load", init);
+// window.addEventListener("load", init);
+init();
