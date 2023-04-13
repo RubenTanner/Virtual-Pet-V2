093aaf2859d377165cd023bece131fc718cf025c
RubenTanner <ruben.sgtech@gmail.com>
2023-04-13 12:16:24 +0100

    Update pet.js

--- webpages/pet.js
+++ webpages/pet.js
@@ -1,24 +1,37 @@
 // A class for the pet
 export class Pet {
-  constructor(name, owner) {
+  constructor(name, owner) { // Constructor for the pet
     this.name = name;
     this.owner = owner;
     this.hunger = 5;
     this.sleep = 5;
     this.happiness = 5;
+    this.cleanliness = 5;
     this.alive = true;
   }
+  
   // Check if the pet is alive or dead
   isAlive() {
-    return this.hunger > 0 && this.sleep > 0 && this.happiness > 0;
+    return this.hunger > 0 && this.sleep > 0 && this.happiness > 0 && this.cleanliness > 0;
   }
+  
   // Set the pet's stats to decay over time
   setDecay() {
     this.hunger--;
     this.sleep--;
     this.happiness--;
+    this.cleanliness--;
     if (!this.isAlive()) {
       this.alive = false;
     }
+  }
+  
+  // Reset the pet's stats to their initial values and set the pet as alive
+  resetGame() {
+    this.hunger = 5;
+    this.sleep = 5;
+    this.happiness = 5;
+    this.cleanliness = 5;
+    this.alive = true;
   }
 }
