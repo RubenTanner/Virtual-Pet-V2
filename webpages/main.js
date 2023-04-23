5ab7bcd906de8eee1a7717527746ffb023f7d940
RubenTanner <ruben.sgtech@gmail.com>
2023-04-23 20:06:19 +0100

    please god i hope this passes

--- webpages/main.js
+++ webpages/main.js
@@ -37,14 +37,21 @@ function init() {
   //Add event listeners for the buttons on the page
   const submitBtn = document.querySelector("#submit");
   submitBtn.addEventListener("click", playGame);
+  document.querySelector("#submit").addEventListener("click", () => {
+    document.querySelector("#submit").textContent = "Try Again";
+  });
   document.querySelector("#eat").addEventListener("click", () => {
     feed();
   });
-  document.querySelector("#sleep").addEventListener("click", () => {
+  document.querySelector("#bed").addEventListener("click", () => {
     sleep();
   });
   document.querySelector("#play").addEventListener("click", () => {
     play();
+    counter();
+  });
+  document.querySelector("#clean").addEventListener("click", () => {
+    clean();
   });
 }
 // Function to start the game
@@ -60,19 +67,25 @@ function playGame() {
 
   console.log(pet);
   // Update the pet's stats on the page
-  document.querySelector("#pet-name").textContent = pet.name;
+  document.querySelectorAll("#pet-name").textContent = pet.name;
   document.querySelector("#hunger").textContent = pet.hunger;
   document.querySelector("#sleep").textContent = pet.sleep;
   document.querySelector("#happiness").textContent = pet.happiness;
-  // debugger;
-  document.getElementById('hungerMeter').value = pet.hunger;
-  document.getElementById('sleepMeter').value = pet.sleep;
-  document.getElementById('happinessMeter').value = pet.happiness;
+  document.querySelector("#cleanliness").textContent = pet.cleanliness;
+  document.querySelectorAll("#owner").textContent = pet.owner;
+  // display the pets stats in a meter
+  document.getElementById("hungerMeter").value = pet.hunger;
+  document.getElementById("sleepMeter").value = pet.sleep;
+  document.getElementById("happinessMeter").value = pet.happiness;
+  document.getElementById("cleanlinessMeter").value = pet.cleanliness;
+
   // Show the stats div
   document.getElementById("stats").style.display = "block";
+  // hide the start div
+  document.getElementById("start").style.display = "none";
 
-   // Set a timer to update the pet's stats and image every second
-   setInterval(() => {
+  // Set a timer to update the pet's stats and image every other second
+  setInterval(() => {
     if (pet.isAlive()) {
       pet.setDecay();
       updatePetImage();
@@ -80,18 +93,44 @@ function playGame() {
       document.querySelector("#hunger").textContent = pet.hunger;
       document.querySelector("#sleep").textContent = pet.sleep;
       document.querySelector("#happiness").textContent = pet.happiness;
+      document.querySelector("#cleanliness").textContent = pet.cleanliness;
+      document.getElementById("hungerMeter").value = pet.hunger;
+      document.getElementById("sleepMeter").value = pet.sleep;
+      document.getElementById("happinessMeter").value = pet.happiness;
+      document.getElementById("cleanlinessMeter").value = pet.cleanliness;
     } else {
       // If the pet is dead, stop the timer and show a message
-      clearInterval(timer);
-      document.querySelector("#dead").textContent = "Your pet is dead!";
+      clearInterval();
+      document.getElementById("stats").style.display = "none";
+      document.querySelector("#dead").style.display = "block";
+      // show the start div again
+      document.getElementById("start").style.display = "block";
+    }
+    // if pet happiness is less than 4, pet is sad
+    if (pet.happiness <= 5) {
+      currentState = "sad";
+    }
+  }, 2000);
+}
+//function that creates a counter for every second the pet is alive
+function counter() {
+  let seconds = 0;
+  setInterval(() => {
+    if (pet.isAlive()) {
+      seconds++;
+    } else {
+      clearInterval();
+      document.querySelector("#counter").textContent = seconds;
     }
   }, 1000);
 }
 // Feed the pet
 function feed() {
+  let timesFed = 0;
   currentState = "eating";
   pet.hunger += 2;
   pet.happiness += 1;
+  timesFed++;
   if (pet.hunger > 8) {
     currentState = "sad";
   } else if (pet.hunger > 4) {
@@ -100,27 +139,44 @@ function feed() {
 }
 
 function play() {
+  let timesPlayed = 0;
   currentState = "playing";
   pet.happiness += 3;
   pet.hunger -= 1;
   pet.sleep -= 1;
+  pet.cleanliness -= 1;
+  timesPlayed++;
   if (pet.happiness > 8) {
-    currentState = "happy";
+    currentState = "playing";
   } else {
     currentState = "idle";
   }
 }
 
 function sleep() {
+  let timesSlept = 0;
   currentState = "sleeping";
   pet.sleep += 2;
   pet.happiness += 1;
+  timesSlept++;
   if (pet.sleep > 8) {
-    currentState = "happy";
+    currentState = "sleeping";
   } else {
     currentState = "idle";
   }
 }
 
+function clean() {
+  let timesCleaned = 0;
+  currentState = "cleaning";
+  pet.cleanliness += 2;
+  pet.happiness += 1;
+  timesCleaned++;
+  if (pet.cleanliness > 8) {
+    currentState = "happy";
+  } else {
+    currentState = "idle";
+  }
+}
 // window.addEventListener("load", init);
 init();
--- webpages/pet.js
+++ webpages/pet.js
@@ -1,6 +1,7 @@
 // A class for the pet
 export class Pet {
-  constructor(name, owner) { // Constructor for the pet
+  constructor(name, owner) {
+    // Constructor for the pet
     this.name = name;
     this.owner = owner;
     this.hunger = 5;
@@ -9,12 +10,17 @@ export class Pet {
     this.cleanliness = 5;
     this.alive = true;
   }
-  
+
   // Check if the pet is alive or dead
   isAlive() {
-    return this.hunger > 0 && this.sleep > 0 && this.happiness > 0 && this.cleanliness > 0;
+    return (
+      this.hunger > 0 &&
+      this.sleep > 0 &&
+      this.happiness > 0 &&
+      this.cleanliness > 0
+    );
   }
-  
+
   // Set the pet's stats to decay over time
   setDecay() {
     this.hunger--;
@@ -24,14 +30,5 @@ export class Pet {
     if (!this.isAlive()) {
       this.alive = false;
     }
-  }
-  
-  // Reset the pet's stats to their initial values and set the pet as alive
-  resetGame() {
-    this.hunger = 5;
-    this.sleep = 5;
-    this.happiness = 5;
-    this.cleanliness = 5;
-    this.alive = true;
   }
 }
